691130b2d802dc875e390d90c1a250c9d09463ae
oleiade <theo@crevon.me>
2022-08-17 08:54:09 +0200

    Add redis example

---
+++ examples/redis/redis.go
@@ -0,0 +1,397 @@
+// Package redis demonstrates the usage of the gomme package to parse Redis'
+// [RESP protocol] messages.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+package redis
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/oleiade/gomme"
+)
+
+// ParseRESPMESSAGE parses a Redis' [RESP protocol] message.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+func ParseRESPMessage(input string) (RESPMessage, error) {
+	if len(input) < 3 {
+		return RESPMessage{}, fmt.Errorf("malformed message %s; reason: %w", input, ErrMessageTooShort)
+	}
+
+	if !isValidMessageKind(MessageKind(input[0])) {
+		return RESPMessage{}, fmt.Errorf("malformed message %s; reason: %w %c", input, ErrInvalidPrefix, input[0])
+	}
+
+	if input[len(input)-2] != '\r' || input[len(input)-1] != '\n' {
+		return RESPMessage{}, fmt.Errorf("malformed message %s; reason: %w", input, ErrInvalidSuffix)
+	}
+
+	parser := gomme.Alternative(
+		SimpleString(),
+		Error(),
+		Integer(),
+		BulkString(),
+		Array(),
+	)
+
+	result := parser([]rune(input))
+	if result.Err != nil {
+		return RESPMessage{}, result.Err
+	}
+
+	return result.Output.(RESPMessage), nil
+}
+
+// ErrMessageTooShort is returned when a message is too short to be valid.
+// A [RESP protocol] message is at least 3 characters long: the message kind
+// prefix, the message content (which can be empty), and the CRLF suffix.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+var ErrMessageTooShort = errors.New("message too short")
+
+// ErrInvalidPrefix is returned when a message kind prefix is not recognized.
+// Valid [RESP Protocol] message kind prefixes are "+", "-", ":", and "$".
+//
+// [RESP Protocol]: https://redis.io/docs/reference/protocol-spec/
+var ErrInvalidPrefix = errors.New("invalid message prefix")
+
+// ErrInvalidSuffix is returned when a message suffix is not recognized.
+// Every [RESP protocol] message ends with a CRLF.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+var ErrInvalidSuffix = errors.New("invalid message suffix")
+
+// RESPMessage is a parsed Redis' [RESP protocol] message.
+//
+// It can hold either a simple string, an error, an integer, a bulk string,
+// or an array. The kind of the message is available in the Kind field.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+type RESPMessage struct {
+	Kind         MessageKind
+	SimpleString *SimpleStringMessage
+	Error        *ErrorStringMessage
+	Integer      *IntegerMessage
+	BulkString   *BulkStringMessage
+	Array        *ArrayMessage
+}
+
+// MessageKind is the kind of a Redis' [RESP protocol] message.
+type MessageKind rune
+
+// The many different kinds of Redis' [RESP protocol] messages map
+// to their respective protocol message's prefixes.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+const (
+	SimpleStringKind MessageKind = '+'
+	ErrorKind        MessageKind = '-'
+	IntegerKind      MessageKind = ':'
+	BulkStringKind   MessageKind = '$'
+	ArrayKind        MessageKind = '*'
+	InvalidKind      MessageKind = '?'
+)
+
+// SimpleStringMessage is a simple string message parsed from a Redis'
+// [RESP protocol] message.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+type SimpleStringMessage struct {
+	Content string
+}
+
+// simpleString is a parser for Redis' RESP protocol simple strings.
+//
+// Simple strings are strings that are not expected to contain newlines.
+// Simple strings start with a "+" character, and end with a CRLF.
+//
+// Once parsed, the content of the simple string is available in the
+// simpleString field of the result's RESPMessage.
+func SimpleString() gomme.Parser {
+	mapFn := func(output any) (any, error) {
+		messageContent, ok := output.(string)
+		if !ok || strings.ContainsAny(messageContent, "\r\n") {
+			return RESPMessage{}, fmt.Errorf("malformed simple string: %s", messageContent)
+		}
+
+		return RESPMessage{
+			Kind: SimpleStringKind,
+			SimpleString: &SimpleStringMessage{
+				Content: messageContent,
+			},
+		}, nil
+	}
+
+	return gomme.Delimited(
+		gomme.Char(rune(SimpleStringKind)),
+		gomme.Map(gomme.TakeUntil(gomme.CRLF()), mapFn),
+		gomme.CRLF(),
+	)
+}
+
+// ErrorStringMessage is a parsed error string message from a Redis'
+// [RESP protocol] message.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+type ErrorStringMessage struct {
+	Kind    string
+	Message string
+}
+
+// Error is a parser for Redis' RESP protocol errors.
+//
+// Errors are strings that start with a "-" character, and end with a CRLF.
+//
+// The error message is available in the Error field of the result's
+// RESPMessage.
+func Error() gomme.Parser {
+	mapFn := func(output any) (any, error) {
+		messageContent, ok := output.(string)
+		if !ok || strings.ContainsAny(messageContent, "\r\n") {
+			return RESPMessage{}, fmt.Errorf("malformed error string: %s", messageContent)
+		}
+
+		return RESPMessage{
+			Kind: ErrorKind,
+			Error: &ErrorStringMessage{
+				Kind:    "ERR",
+				Message: messageContent,
+			},
+		}, nil
+	}
+
+	return gomme.Delimited(
+		gomme.Char(rune(ErrorKind)),
+		gomme.Map(gomme.TakeUntil(gomme.CRLF()), mapFn),
+		gomme.CRLF(),
+	)
+}
+
+// IntegerMessage is a parsed integer message from a Redis' [RESP protocol]
+// message.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+type IntegerMessage struct {
+	Value int
+}
+
+// Integer is a parser for Redis' RESP protocol integers.
+//
+// Integers are signed nummerical values represented as string messages
+// that start with a ":" character, and end with a CRLF.
+//
+// The integer value is available in the IntegerMessage field of the result's
+// RESPMessage.
+func Integer() gomme.Parser {
+	mapFn := func(output any) (any, error) {
+		value, err := strconv.Atoi(output.(string))
+		if err != nil {
+			return RESPMessage{}, err
+		}
+
+		return RESPMessage{
+			Kind: IntegerKind,
+			Integer: &IntegerMessage{
+				Value: value,
+			},
+		}, nil
+	}
+
+	return gomme.Delimited(
+		gomme.Char(rune(IntegerKind)),
+		gomme.Map(gomme.TakeUntil(gomme.CRLF()), mapFn),
+		gomme.CRLF(),
+	)
+}
+
+// BulkStringMessage is a parsed bulk string message from a Redis' [RESP protocol]
+// message.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+type BulkStringMessage struct {
+	Data []byte
+}
+
+// BulkString is a parser for Redis' RESP protocol bulk strings.
+//
+// Bulk strings are binary-safe strings up to 512MB in size.
+// Bulk strings start with a "$" character, and end with a CRLF.
+//
+// The bulk string's data is available in the BulkString field of the result's
+// RESPMessage.
+func BulkString() gomme.Parser {
+	mapFn := func(output any) (any, error) {
+		pair, ok := output.([]interface{})
+		if !ok {
+			return RESPMessage{}, fmt.Errorf("malformed bulk string; " +
+				"reason unable to process parser's resulting type")
+		}
+
+		length, ok := pair[0].(float64)
+		if !ok {
+			return RESPMessage{}, fmt.Errorf(
+				"unable to parse bulk string; "+
+					"reason: unable to parse length component %f",
+				length,
+			)
+		}
+
+		// FIXME: this should cast to []byte, thus
+		// we need a Bytes() combinator.
+		data, dataOk := pair[1].(string)
+		if int(length) < 0 {
+			if int(length) < -1 {
+				return RESPMessage{}, fmt.Errorf(
+					"unable to parse bulk string; "+
+						"reason: negative length %f",
+					length,
+				)
+			}
+
+			if int(length) == -1 && len(data) != 0 {
+				return RESPMessage{}, fmt.Errorf(
+					"malformed array: declared message size -1, and actual size differ %d",
+					len(data),
+				)
+			}
+		} else {
+			if !dataOk {
+				return RESPMessage{}, fmt.Errorf("unable to parse bulk string; "+
+					"reason: unable to parse data component %s", data)
+			}
+
+			if len(data) != int(length) {
+				return RESPMessage{}, fmt.Errorf(
+					"malformed array: declared message size %f, and actual size differ %d",
+					length,
+					len(data),
+				)
+			}
+		}
+
+		return RESPMessage{
+			Kind: BulkStringKind,
+			BulkString: &BulkStringMessage{
+				Data: []byte(data),
+			},
+		}, nil
+	}
+
+	return gomme.Map(
+		gomme.Pair(
+			sizePrefix(gomme.Char(rune(BulkStringKind))),
+			gomme.Optional(
+				gomme.Terminated(gomme.TakeUntil(gomme.CRLF()), gomme.CRLF()),
+			),
+		),
+		mapFn,
+	)
+}
+
+// ArrayMessage is a parsed array message from a Redis' [RESP protocol] message.
+//
+// [RESP protocol]: https://redis.io/docs/reference/protocol-spec/
+type ArrayMessage struct {
+	Elements []RESPMessage
+}
+
+// Array is a parser for Redis' RESP protocol arrays.
+//
+// Arrays are sequences of RESP messages.
+// Arrays start with a "*" character, and end with a CRLF.
+//
+// The array's messages are available in the Array field of the result's
+// RESPMessage.
+func Array() gomme.Parser {
+	mapFn := func(output any) (any, error) {
+		pair, ok := output.([]interface{})
+		if !ok {
+			return RESPMessage{}, fmt.Errorf("malformed array message; " +
+				"reason: unable to process parser's resulting type")
+		}
+
+		messageCount, ok := pair[0].(float64)
+		if !ok {
+			return RESPMessage{}, fmt.Errorf(
+				"unable to parse array size from message; "+
+					"reason: unable to parse size component %f",
+				messageCount,
+			)
+		}
+
+		outputs, ok := pair[1].([]interface{})
+		if !ok {
+			return RESPMessage{}, fmt.Errorf("unable to parse array message; "+
+				"reason: unable to parse components %v", outputs)
+		}
+
+		if int(messageCount) == -1 {
+			if len(outputs) != 0 {
+				return RESPMessage{}, fmt.Errorf(
+					"malformed array: declared message size -1, and actual size differ %d",
+					len(outputs),
+				)
+			}
+		} else {
+			if len(outputs) != int(messageCount) {
+				return RESPMessage{}, fmt.Errorf(
+					"malformed array: declared message size %f, and actual size differ %d",
+					messageCount,
+					len(outputs),
+				)
+			}
+		}
+
+		messages := make([]RESPMessage, 0, len(outputs))
+		for _, message := range outputs {
+			messages = append(messages, message.(RESPMessage))
+		}
+
+		return RESPMessage{
+			Kind: ArrayKind,
+			Array: &ArrayMessage{
+				Elements: messages,
+			},
+		}, nil
+	}
+
+	return gomme.Map(
+		gomme.Pair(
+			sizePrefix(gomme.Char(rune(ArrayKind))),
+			gomme.Many(
+				gomme.Alternative(
+					SimpleString(),
+					Error(),
+					Integer(),
+					BulkString(),
+				),
+			),
+		),
+		mapFn,
+	)
+}
+
+func sizePrefix(prefix gomme.Parser) gomme.Parser {
+	return gomme.Delimited(
+		prefix,
+		gomme.Float(),
+		gomme.CRLF(),
+	)
+}
+
+func prefix() gomme.Parser {
+	return gomme.Alternative(
+		gomme.Char(rune(SimpleStringKind)),
+		gomme.Char(rune(ErrorKind)),
+		gomme.Char(rune(IntegerKind)),
+		gomme.Char(rune(BulkStringKind)),
+		gomme.Char(rune(ArrayKind)),
+	)
+}
+
+func isValidMessageKind(kind MessageKind) bool {
+	return kind == SimpleStringKind || kind == ErrorKind || kind == IntegerKind || kind == BulkStringKind || kind == ArrayKind
+}
---
+++ examples/redis/redis_test.go
@@ -0,0 +1,549 @@
+package redis
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestParseRESPMessage(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    RESPMessage
+		wantErr bool
+	}{
+		//
+		// General
+		//
+		{
+			name: "empty message should fail",
+			args: args{
+				input: "",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "message with only a prefix should fail",
+			args: args{
+				input: "+",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "message with only a CRLF should fail",
+			args: args{
+				input: "\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "message with an invalid prefix should fail",
+			args: args{
+				input: "?\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+
+		//
+		// SIMPLE STRINGS
+		//
+
+		{
+			name: "proper simple string should succeed",
+			args: args{
+				"+OK\r\n",
+			},
+			want: RESPMessage{
+				Kind:         SimpleStringKind,
+				SimpleString: &SimpleStringMessage{Content: "OK"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "empty simple string should succeed",
+			args: args{
+				"+\r\n",
+			},
+			want: RESPMessage{
+				Kind:         SimpleStringKind,
+				SimpleString: &SimpleStringMessage{Content: ""},
+			},
+			wantErr: false,
+		},
+		{
+			name: "malformed simple string containing a \\r should fail",
+			args: args{
+				"+Hello\rWorld\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "malformed simple string containing a \\n should fail",
+			args: args{
+				"+Hello\nWorld\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "malformed simple string containing a \\n\\r should fail",
+			args: args{
+				"+Hello\n\rWorld\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+
+		// //
+		// // ERROR STRINGS
+		// //
+
+		{
+			name: "proper error string should succeed",
+			args: args{
+				"-Error message\r\n",
+			},
+			want: RESPMessage{
+				Kind: ErrorKind,
+				Error: &ErrorStringMessage{
+					Kind:    "ERR",
+					Message: "Error message",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "malformed error string containing a \\r should fail",
+			args: args{
+				"-Error\r message\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "malformed error string containing a \\n should fail",
+			args: args{
+				"-Error\n message\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "malformed error string containing a \\n\\r should fail",
+			args: args{
+				"-Error\n\r message\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+
+		// //
+		// // INTEGER
+		// //
+
+		{
+			name: "proper integer should succeed",
+			args: args{
+				":123\r\n",
+			},
+			want: RESPMessage{
+				Kind: IntegerKind,
+				Integer: &IntegerMessage{
+					Value: 123,
+				},
+			},
+			wantErr: false,
+		},
+
+		//
+		// Bulk Strings
+		//
+
+		{
+			name: "proper bulk string should succeed",
+			args: args{
+				"$5\r\nhello\r\n",
+			},
+			want: RESPMessage{
+				Kind: BulkStringKind,
+				BulkString: &BulkStringMessage{
+					Data: []byte("hello"),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "nil bulk string should succeed",
+			args: args{
+				"$-1\r\n",
+			},
+			want: RESPMessage{
+				Kind: BulkStringKind,
+				BulkString: &BulkStringMessage{
+					Data: []byte(""),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "bulk string with negative size != -1 should fail",
+			args: args{
+				"$-2\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+		{
+			name: "malformed bulk string with actual length different from declared length should fail",
+			args: args{
+				"$5\r\nhello world\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+
+		//
+		// ARRAYS
+		//
+
+		{
+			name: "proper array of simple strings should succeed",
+			args: args{
+				"*2\r\n+hello\r\n+world\r\n",
+			},
+			want: RESPMessage{
+				Kind: ArrayKind,
+				Array: &ArrayMessage{
+					Elements: []RESPMessage{
+						{
+							Kind: SimpleStringKind,
+							SimpleString: &SimpleStringMessage{
+								Content: "hello",
+							},
+						},
+						{
+							Kind: SimpleStringKind,
+							SimpleString: &SimpleStringMessage{
+								Content: "world",
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "proper array of errors should succeed",
+			args: args{
+				"*2\r\n-Error Message\r\n-Other error\r\n",
+			},
+			want: RESPMessage{
+				Kind: ArrayKind,
+				Array: &ArrayMessage{
+					Elements: []RESPMessage{
+						{
+							Kind: ErrorKind,
+							Error: &ErrorStringMessage{
+								Kind:    "ERR",
+								Message: "Error Message",
+							},
+						},
+						{
+							Kind: ErrorKind,
+							Error: &ErrorStringMessage{
+								Kind:    "ERR",
+								Message: "Other error",
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "proper array of integers should succeed",
+			args: args{
+				"*2\r\n:0\r\n:1000\r\n",
+			},
+			want: RESPMessage{
+				Kind: ArrayKind,
+				Array: &ArrayMessage{
+					Elements: []RESPMessage{
+						{
+							Kind: IntegerKind,
+							Integer: &IntegerMessage{
+								Value: 0,
+							},
+						},
+						{
+							Kind: IntegerKind,
+							Integer: &IntegerMessage{
+								Value: 1000,
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "proper array of bulk strings should succeed",
+			args: args{
+				"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n",
+			},
+			want: RESPMessage{
+				Kind: ArrayKind,
+				Array: &ArrayMessage{
+					Elements: []RESPMessage{
+						{
+							Kind: BulkStringKind,
+							BulkString: &BulkStringMessage{
+								Data: []byte("hello"),
+							},
+						},
+						{
+							Kind: BulkStringKind,
+							BulkString: &BulkStringMessage{
+								Data: []byte("world"),
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "proper array of mixed types should succeed",
+			args: args{
+				"*4\r\n$5\r\nhello\r\n:123\r\n+OK\r\n-Error Message\r\n",
+			},
+			want: RESPMessage{
+				Kind: ArrayKind,
+				Array: &ArrayMessage{
+					Elements: []RESPMessage{
+						{
+							Kind: BulkStringKind,
+							BulkString: &BulkStringMessage{
+								Data: []byte("hello"),
+							},
+						},
+						{
+							Kind: IntegerKind,
+							Integer: &IntegerMessage{
+								Value: 123,
+							},
+						},
+						{
+							Kind: SimpleStringKind,
+							SimpleString: &SimpleStringMessage{
+								Content: "OK",
+							},
+						},
+						{
+							Kind: ErrorKind,
+							Error: &ErrorStringMessage{
+								Message: "Error Message",
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "empty array should succeed",
+			args: args{
+				"*-1\r\n",
+			},
+			want: RESPMessage{
+				Kind: ArrayKind,
+				Array: &ArrayMessage{
+					Elements: []RESPMessage{},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "array with non matching size prefix should fail",
+			args: args{
+				"*2\r\n+OK\r\n",
+			},
+			want:    RESPMessage{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseRESPMessage(tt.args.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseRESPMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseRESPMessage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkParseMessage(b *testing.B) {
+	var benchmarks = []struct {
+		kind string
+		data string
+		size string
+	}{
+		{"simple_string", "+OK\r\n", "2"},
+		{"simple_string", simpleStringProducer(128 * Bytes), "128b"},
+		{"simple_string", simpleStringProducer(1 * KiloBytes), "1kb"},
+		{"simple_string", simpleStringProducer(1 * MegaBytes), "1mb"},
+		{"error_string", "-Error\r\n", "5"},
+		{"error_string", errorStringProducer(128 * Bytes), "128b"},
+		{"error_string", errorStringProducer(1 * KiloBytes), "1kb"},
+		{"integer", ":1\r\n", "1"},
+		{"integer", ":9,223,372,036,854,775,807\r\n", "biggest integer"},
+		{"integer", ":-9223372036854775808\r\n", "smallest integer"},
+		{"bulk_string", bulkStringProducer(128 * Bytes), "128b"},
+		{"bulk_string", bulkStringProducer(1 * KiloBytes), "1kb"},
+		{"bulk_string", bulkStringProducer(1 * MegaBytes), "1mb"},
+		{"bulk_string", bulkStringProducer(512 * MegaBytes), "512mb"},
+		{"array", arrayProducer(10000, 128*Bytes), "10000 * 128b"},
+		{"array", arrayProducer(1000, 1*KiloBytes), "1000 * 1kb"},
+		{"array", arrayProducer(100, 1*MegaBytes), "100 * 1mb"},
+	}
+
+	for _, tt := range benchmarks {
+		b.Run(fmt.Sprintf("%s_with_size_%s", tt.kind, tt.size), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				ParseRESPMessage(tt.data)
+			}
+		})
+	}
+}
+
+const (
+	Bytes     = 1
+	KiloBytes = Bytes * 1024
+	MegaBytes = KiloBytes * 1024
+	GigaBytes = MegaBytes * 1024
+	TeraBytes = GigaBytes * 1024
+)
+
+// TODO: add fuzz tests input for other kind of messages,
+// and handled their expected format too.
+func FuzzTestParseMessage(f *testing.F) {
+	testCases := []string{
+		"+OK\r\n",
+		"+Hello world\r\n",
+		"+This is a string\r\n",
+	}
+
+	for _, testCase := range testCases {
+		f.Add(testCase)
+	}
+
+	f.Fuzz(func(t *testing.T, message string) {
+		_, err := ParseRESPMessage(message)
+		if err != nil {
+			if errors.Is(err, ErrMessageTooShort) || errors.Is(err, ErrInvalidPrefix) || errors.Is(err, ErrInvalidSuffix) {
+				t.Skip("skipping expected error")
+			}
+
+			if strings.Count(message, "\r") > 1 || strings.Count(message, "\n") > 1 {
+				t.Skip("skipping simple string message with multiple \\r or \\n")
+			}
+
+			t.Errorf("ParseRESPMessage() error = %v", err)
+		}
+	})
+}
+
+func simpleStringProducer(messageSize int) string {
+	return strings.Join(
+		[]string{
+			"+",
+			stringWithinCharset(messageSize, alnumCharset),
+			"\r\n",
+		},
+		"",
+	)
+}
+
+func errorStringProducer(messageSize int) string {
+	return strings.Join(
+		[]string{
+			"-",
+			stringWithinCharset(messageSize, alnumCharset),
+			"\r\n",
+		},
+		"",
+	)
+}
+
+func bulkStringProducer(messageSize int) string {
+	return strings.Join(
+		[]string{
+			"$",
+			strconv.Itoa(messageSize),
+			"\r\n",
+			stringWithinCharset(messageSize, alnumCharset),
+			"\r\n",
+		},
+		"",
+	)
+}
+
+func arrayProducer(arraySize, messageSize int) string {
+	messages := make([]string, 0, arraySize)
+
+	for i := 0; i < arraySize; i++ {
+		messageKind := i % 4
+
+		switch messageKind {
+		case 0:
+			messages = append(messages, simpleStringProducer(messageSize))
+		case 1:
+			messages = append(messages, errorStringProducer(messageSize))
+		case 2:
+			messages = append(messages, bulkStringProducer(messageSize))
+		case 3:
+			messages = append(messages, strconv.Itoa(rand.Int()))
+		}
+	}
+
+	return strings.Join(
+		[]string{
+			"*",
+			strings.Join(messages, ""),
+			"\r\n",
+		},
+		"",
+	)
+}
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+const alnumCharset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func stringWithinCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
