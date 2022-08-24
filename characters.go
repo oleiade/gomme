e3dc2173f3fa83dfa75774750d45942f1894a198
oleiade <theo@crevon.me>
2022-08-24 17:13:51 +0200

    Add Int8 character combinator

--- characters.go
+++ characters.go
@@ -324,6 +324,26 @@ func Int64[I Bytes]() Parser[I, int64] {
 	}
 }
 
+// Int8 parses an 8-bit integer from the input,
+// and returns the part of the input that matched the integer.
+func Int8[I Bytes]() Parser[I, int8] {
+	return func(input I) Result[int8, I] {
+		parser := Recognize(Sequence(Optional(Token[I]("-")), Digit1[I]()))
+
+		result := parser(input)
+		if result.Err != nil {
+			return Failure[I, int8](NewGenericError(input, "int8"), input)
+		}
+
+		n, err := strconv.ParseInt(string(result.Output), 10, 8)
+		if err != nil {
+			return Failure[I, int8](NewGenericError(input, "int8"), input)
+		}
+
+		return Success(int8(n), result.Remaining)
+	}
+}
+
 // IsAlpha returns true if the rune is an alphabetic character.
 func IsAlpha(c rune) bool {
 	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
--- characters_test.go
+++ characters_test.go
@@ -1417,3 +1417,71 @@ func TestInt64(t *testing.T) {
 		})
 	}
 }
+
+func TestInt8(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name          string
+		parser        Parser[string, int8]
+		input         string
+		wantErr       bool
+		wantOutput    int8
+		wantRemaining string
+	}{
+		{
+			name:          "parsing positive integer should succeed",
+			parser:        Int8(),
+			input:         "123",
+			wantErr:       false,
+			wantOutput:    123,
+			wantRemaining: "",
+		},
+		{
+			name:          "parsing negative integer should succeed",
+			parser:        Int8(),
+			input:         "-123",
+			wantErr:       false,
+			wantOutput:    -123,
+			wantRemaining: "",
+		},
+		{
+			name:          "parsing positive integer prefix should succeed",
+			parser:        Int8(),
+			input:         "123abc",
+			wantErr:       false,
+			wantOutput:    123,
+			wantRemaining: "abc",
+		},
+		{
+			name:          "parsing negative integer should succeed",
+			parser:        Int8(),
+			input:         "-123abc",
+			wantErr:       false,
+			wantOutput:    -123,
+			wantRemaining: "abc",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotResult := tc.parser(tc.input)
+			if (gotResult.Err != nil) != tc.wantErr {
+				t.Errorf("got error %v, want error %v", gotResult.Err, tc.wantErr)
+			}
+
+			if gotResult.Output != tc.wantOutput {
+				t.Errorf("got output %v, want output %v", gotResult.Output, tc.wantOutput)
+			}
+
+			if gotResult.Remaining != tc.wantRemaining {
+				t.Errorf("got remaining %v, want remaining %v", gotResult.Remaining, tc.wantRemaining)
+			}
+		})
+	}
+}
+
